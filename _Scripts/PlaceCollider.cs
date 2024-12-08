3949b169b5bc85ae7c20295059f2fe37cc3822e8
ppotho <classicalfeynman@gmail.com>
2024-12-08 09:25:33 +0900

    Table Collider

---
+++ _Scripts/PlaceCollider.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceCollider : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+    }
+}
